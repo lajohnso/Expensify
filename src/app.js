5abef338daf0964b73331afa9b230125328320fd
Lars-August Udnesseter Johnson <Lars-August@lars-augusts-macbook-pro.home>
2020-12-17 18:22:51 +0100

    added init router

--- src/app.js
+++ src/app.js
@@ -1,6 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 import "normalize.css/normalize.css";
 import "./styles/styles.scss";
 
-ReactDOM.render(<p>This is my boilerplate</p>, document.getElementById("app"));
+const ExpenceDashboardPage = () => <div>This is content from dashboard</div>;
+
+const AddExpensePage = () => <div>This is content from Add expense</div>;
+
+const EditExpensePage = () => <div>This is content from Edit</div>;
+
+const HelpPage = () => <div>This is content from Help</div>;
+
+const routes = (
+  <BrowserRouter>
+    <div>
+      <Route exact path='/' component={ExpenceDashboardPage} />
+      <Route path='/create' component={AddExpensePage} />
+      <Route path='/edit' component={EditExpensePage} />
+      <Route path='/help' component={HelpPage} />
+    </div>
+  </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById("app"));
